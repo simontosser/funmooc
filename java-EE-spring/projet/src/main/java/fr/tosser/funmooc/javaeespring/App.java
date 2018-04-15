d2eec51cabee4054acab803633cbdf17c7187075
Simon TOSSER <simon@kornog-computing.com>
2018-04-15 16:36:54 +0200

    create project

---
+++ java-EE-spring/projet/src/main/java/fr/tosser/funmooc/javaeespring/App.java
@@ -0,0 +1,13 @@
+package fr.tosser.funmooc.javaeespring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
